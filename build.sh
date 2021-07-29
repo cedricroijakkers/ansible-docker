8566003775cb1833bd05680467c94556cb13ca1f
martijn.suijlen <msuijlen@ilionx.com>
2021-07-29 10:23:32 +0200

    Added ansible-galaxy community.general to the default Ansible installation

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -e
 # Configure the base name of the image to build here
 containerbase="cedricroijakkers/ansible-docker"
 
-cat images.csv | while IFS=',' read -r pythonver ansiblever template
+cat images.csv | while IFS=',' read -r pythonver ansiblever template galaxycomgenver
 do
     # If the line starts with a hash, it's a comment and skip it
     if [[ ${pythonver::1} == "#" ]]
@@ -17,7 +17,7 @@ do
     # Else create a directory and generate a Dockerfile
     echo -e "\e[36mGenerating Dockerfile for image ${ansiblever}-${pythonver}...\e[39m"
     mkdir -p "${ansiblever}-${pythonver}"
-    cat "${template}" | sed -e "s/%%PLACEHOLDER_PYTHON_IMAGE%%/${pythonver}/g" | sed -e "s/%%PLACEHOLDER_ANSIBLE_VERSION%%/${ansiblever}/g" > "${ansiblever}-${pythonver}/Dockerfile"
+    cat "${template}" | sed -e "s/%%PLACEHOLDER_PYTHON_IMAGE%%/${pythonver}/g" | sed -e "s/%%PLACEHOLDER_ANSIBLE_VERSION%%/${ansiblever}/g" | sed -e "s/%%PLACEHOLDER_GALAXY_COM_GEN_VERSION%%/${galaxycomgenver}/g" > "${ansiblever}-${pythonver}/Dockerfile"
     echo -e "\e[32mDockerfile generation: DONE\e[39m"
 
     # And build the docker container in question
